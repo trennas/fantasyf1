cf1dccc5e904bdb16e584ccaa119de0ddc9444e2
tp23838 <michael.trenaman@lv.com>
2015-11-06 11:46:24 +0000

    Begin views

--- src/main/java/net/ddns/f1/web/MainController.java
+++ src/main/java/net/ddns/f1/web/MainController.java
@@ -1,6 +1,14 @@
 package net.ddns.f1.web;
 
+import java.util.List;
+
+import net.ddns.f1.domain.Team;
+import net.ddns.f1.repository.TeamRepository;
+import net.ddns.f1.service.impl.LeagueServiceImpl;
+import net.ddns.f1.service.impl.TeamService;
+
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,10 +20,28 @@ public class MainController {
 
 	private static final Logger LOG = Logger
 			.getLogger(MainController.class);
+	
+	@Autowired
+	private LeagueServiceImpl leagueService;
+	@Autowired
+	private TeamRepository teamRepo;
+	@Autowired
+	private TeamService teamService;
 
 	@RequestMapping("/")
-	@ResponseBody
 	public String mainPage() {
-		return "Welcome to FF1!";
+		return "league";
+	}
+	
+	@RequestMapping("/teams")
+	@ResponseBody
+	public List<Team> getTeams() {
+		return leagueService.calculateLeagueStandings();
+	}
+	
+	@RequestMapping("/team")
+	@ResponseBody
+	public Team getTeam(String name) {
+		return teamRepo.findTeamByName(name).get(0);
 	}
 }
