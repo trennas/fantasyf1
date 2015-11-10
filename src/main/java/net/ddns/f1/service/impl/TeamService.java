a6ae43352917b39e7ed0cfb3a283a37e0436310b
tp23838 <michael.trenaman@lv.com>
2015-11-10 11:36:27 +0000

    Remove £

--- src/main/java/net/ddns/f1/service/impl/TeamService.java
+++ src/main/java/net/ddns/f1/service/impl/TeamService.java
@@ -68,7 +68,7 @@ public class TeamService {
 		cost += team.getEngine().getPrice();
 
 		if (cost > budget) {
-			throw new ValidationException("Team is over budget by £"
+			throw new ValidationException("Team is over budget by "
 					+ (cost - budget) + "m");
 		}
 	}
