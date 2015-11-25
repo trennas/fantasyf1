793a47f2d4b8de53736dbe312864e5d798cea34b
tp23838 <michael.trenaman@lv.com>
2015-11-25 09:51:41 +0000

    Mail service update

--- src/main/java/net/ddns/f1/service/impl/MailServiceImpl.java
+++ src/main/java/net/ddns/f1/service/impl/MailServiceImpl.java
@@ -11,14 +11,12 @@ import javax.mail.internet.MimeMessage;
 
 import net.ddns.f1.domain.EventResult;
 import net.ddns.f1.domain.Team;
-import net.ddns.f1.repository.ConfigRepository;
 import net.ddns.f1.repository.TeamRepository;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
@@ -28,72 +26,65 @@ public class MailServiceImpl {
 			.getLogger(MailServiceImpl.class);
 	@Autowired
 	JavaMailSender mailSender;
-	
+
 	@Autowired
 	TeamRepository teamRepo;
-	
-	@Autowired
-	ConfigRepository configRepo;
-	
+
 	@Value("${website-url}")
 	private String websiteUrl;
-	
+
 	@Value("${new-result-email-alerts}")
 	private boolean newResultEmailAlerts;
-	
+
 	@Value("${qualifying-result-email-alerts}")
 	private boolean qualifyingResultEmailAlerts;
-	
-	public void sendNewResultsMail(EventResult result) {
+
+	public void sendNewResultsMail(final EventResult result) {
 		try {
-			if(newResultEmailAlerts && (qualifyingResultEmailAlerts || result.isRaceComplete())) {
-				if(mailSender instanceof JavaMailSenderImpl) {
-					JavaMailSenderImpl mailSenderImpl = (JavaMailSenderImpl) mailSender;
-					mailSenderImpl.setPassword(configRepo.findByKey("emailPassword").get(0).getValue());
-				}			
-				
-		        F1MimeMessagePreparator mimePrep = new F1MimeMessagePreparator(result, teamRepo.findAll().iterator());			
-				//F1MimeMessagePreparator mimePrep = new F1MimeMessagePreparator(result, teamRepo.findByEmail("mike.trenaman@gmail.com").iterator());			
-		        
+			if (newResultEmailAlerts
+					&& (qualifyingResultEmailAlerts || result.isRaceComplete())) {
+		        final F1MimeMessagePreparator mimePrep = new F1MimeMessagePreparator(result, teamRepo.findAll().iterator());
+				//F1MimeMessagePreparator mimePrep = new F1MimeMessagePreparator(result, teamRepo.findByEmail("mike.trenaman@gmail.com").iterator());
+
 		        mailSender.send(mimePrep);
 		        LOG.info("Sent new" + (result.isRaceComplete() ? " Race " : " Qualifying ") + "results email for " + result.getVenue());
 			}
-		} catch(Exception e) {
+		} catch(final Exception e) {
 			LOG.error("Error sending mail. " + e.getClass().getName() + ": " + e.getMessage());
 		}
 	}
-	
+
 	private class F1MimeMessagePreparator implements MimeMessagePreparator {
-		
-		private EventResult result;
-		private Iterator<Team> itr;
-		
-    	public F1MimeMessagePreparator(EventResult result, Iterator<Team> itr) {
+
+		private final EventResult result;
+		private final Iterator<Team> itr;
+
+    	public F1MimeMessagePreparator(final EventResult result, final Iterator<Team> itr) {
     		this.result = result;
     		this.itr = itr;
     	}
-    	
+
     	@Override
-        public void prepare(MimeMessage mimeMessage) throws Exception {
-			List<Address> addresses = new ArrayList<Address>();
+        public void prepare(final MimeMessage mimeMessage) throws Exception {
+			final List<Address> addresses = new ArrayList<Address>();
 			while(itr.hasNext()) {
 				addresses.add(new InternetAddress(itr.next().getEmail()));
 			}
-			
-			Address[] addressArray = new Address[addresses.size()];
-			
+
+			final Address[] addressArray = new Address[addresses.size()];
+
     		mimeMessage.addRecipients(Message.RecipientType.TO, addresses.toArray(addressArray));
         	mimeMessage.setSubject("Fantasy F1 - " + result.getVenue() + (result.isRaceComplete() ? " Race " : " Qualifying ") + "Results");
-        	
-        	String message = "<font face=\"arial\"><h4>Results From The " + result.getVenue() + "</h4>" +
+
+        	final String message = "<font face=\"arial\"><h4>Results From The " + result.getVenue() + "</h4>" +
         					 "<p>" + (result.isRaceComplete() ? "Race " : "Qualifying ")
         					 + "Results from the " + result.getVenue() + " have been received "
         					 + "and scores have been updated.</p>"
         					 + "<p>Visit the Fantasy F1 Webpage to check the latest scores: -</p><br/>"
         					 + "<a href=\"" + websiteUrl + "\">" + websiteUrl + "</a></font>";
-        	
-        	String htmlMessage = "<!DOCTYPE html><html><head/><body>" + message + "</body></html>";
-			mimeMessage.setContent(htmlMessage, "text/html; charset=utf-8");			
+
+        	final String htmlMessage = "<!DOCTYPE html><html><head/><body>" + message + "</body></html>";
+			mimeMessage.setContent(htmlMessage, "text/html; charset=utf-8");
         }
     }
 }
